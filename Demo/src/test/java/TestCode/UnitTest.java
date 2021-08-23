7834a8d51383e62cc59e346c2cb2a7e0fc0f6566
PradeepK619 <pkb619@gmail.com>
2021-08-23 11:30:30 +0530

    Added some console output code

--- Demo/src/test/java/TestCode/UnitTest.java
+++ Demo/src/test/java/TestCode/UnitTest.java
@@ -11,24 +11,28 @@ public class UnitTest {
 		
 		c=a+b;
 		Assert.assertEquals(c, 30.0);
+		System.out.println("Addition passed");
 	}
 	@Test
 	public void diffTest() {
 		double a=10,b=20,c;
 		c=a-b;
 		Assert.assertEquals(c, -10.0);
+		System.out.println("Subtraction passed");
 	}
 	@Test
 	public void mulTest() {
 		double a=10,b=20,c;
 		c=a*b;
 		Assert.assertEquals(c, 200.0);
+		System.out.println("Multiplication passed");
 	}
 	@Test
 	public void divTest() {
 		double a=10,b=20,c;
 		c=a/b;
 		Assert.assertEquals(c, 0.5);
+		System.out.println("Division passed");
 	}
 	
 }
